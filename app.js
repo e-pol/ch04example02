4869ac6f933c08de2ebefb5994a90cb7484818b6
e.pol <eugene.poletski@gmail.com>
2015-08-17 01:53:13 +0300

    Refactor: removed duplicated code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var main = function() {
+var main = function () {
 	"use strict";
 
-	$(".comment-input button").on("click", function(event) {
-		var $new_comment = $("<p>");
-		
+	var addCommentFromInputBox = function () {
+		var $new_comment;
+
 		if ($(".comment-input input").val() !== "") {
 			$new_comment = $("<p>").text($(".comment-input input").val());
 			$new_comment.hide();
@@ -11,22 +11,17 @@ var main = function() {
 			$new_comment.fadeIn();
 			$(".comment-input input").val("");
 		}
+	};
+
+	$(".comment-input button").on("click", function (event) {
+		addCommentFromInputBox();
 	});
 
 	$(".comment-input input").on("keypress", function(event) {
-		var $new_comment;
-		
 		if (event.keyCode === 13) {
-			if ($(".comment-input input").val() !== "") {
-				$new_comment = $("<p>").text($(".comment-input input").val());
-				$new_comment.hide();
-				$(".comments").append($new_comment);
-				$new_comment.fadeIn();
-				$(".comment-input input").val("");
-			}
+			addCommentFromInputBox();
 		}
 	});
-
 };
 
 $(document).ready(main);
