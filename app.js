f32e7394fc50fc0f2c846a92ad89308af18bc476
e.pol <eugene.poletski@gmail.com>
2015-08-17 01:41:49 +0300

    Add keypress===13 handler.

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ var main = function() {
 		if ($(".comment-input input").val() !== "") {
 			$new_comment = $("<p>").text($(".comment-input input").val());
 			$(".comments").append($new_comment);
+			$(".comment-input input").val("");
 		}
 	});
+
+	$(".comment-input input").on("keypress", function(event) {
+		var $new_comment;
+		
+		if (event.keyCode === 13) {
+			if ($(".comment-input input").val() !== "") {
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$(".comments").append($new_comment);
+				$(".comment-input input").val("");
+			}
+		}
+	});
+
 };
 
 $(document).ready(main);
